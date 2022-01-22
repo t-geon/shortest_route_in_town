a992fff07ffb7f8956751a9bbb7c10a0167fcd2c
t-geon <88877637+t-geon@users.noreply.github.com>
2022-01-22 13:16:52 +0900

    Update Edge.cpp

--- prac/Edge.cpp
+++ prac/Edge.cpp
@@ -6,23 +6,23 @@ Edge::Edge() {
 	m_pNext = NULL;
 }
 
-/// copy constructor
+// copy constructor
 Edge::Edge(int key, int weight) {
 	m_key = key;
 	m_weight = weight;
 	m_pNext = NULL;
 }
 
-/// set the next pointer of this edge
+// set the next pointer of this edge
 void Edge::SetNext(Edge* pNext) {
 	m_pNext = pNext;
 }
 
-/// get the key of this edge
+// get the key of this edge
 int Edge::GetKey() const {return m_key;}
 
-/// get the weight of this edge
+// get the weight of this edge
 int Edge::GetWeight() const {return m_weight;}
 
-/// get the next pointer of this edge
+// get the next pointer of this edge
 Edge* Edge::GetNext() const {return m_pNext;}
