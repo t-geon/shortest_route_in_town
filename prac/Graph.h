515e0fb14149a31cc7fadb3699c87e6d9ca3d788
t-geon <88877637+t-geon@users.noreply.github.com>
2022-01-25 09:54:11 +0900

    Update Graph.h

--- prac/Graph.h
+++ prac/Graph.h
@@ -22,49 +22,47 @@ private:
     int m_vSize;
 
 public:
-    /// constructor
+    // constructor
     Graph();
-    /// destructor
+    // destructor
     ~Graph();
 
-
-    //내가 추가한거
-    void AddName(int vertexKey, string name);//이름 추가하는 함수
-    void Addstore(int vertexKey, string store);//이름 추가하는 함수
+    void AddName(int vertexKey, string name);//add name function
+    void Addstore(int vertexKey, string store);//add name function
     Vertex* head() { return m_pVHead; };
 
 
-    /// add vertex with vertexNum at the end of the linked list for the vertics
+    // add vertex with vertexNum at the end of the linked list for the vertics
     void AddVertex(int vertexKey);
 
-    /// add edge from the vertex which the number is startVertexKey to the vertex which the number is endVertexKey
+    // add edge from the vertex which the number is startVertexKey to the vertex which the number is endVertexKey
     void AddEdge(int startVertexKey, int endVertexKey, int weight);
 
-    /// get the vertex which the key is vertexNum
+    // get the vertex which the key is vertexNum
     Vertex* FindVertex(int key);
 
-    /// get the number of the vertics
+    // get the number of the vertics
     int Size() const;
 
-    /// memory free for the vertics
+    // memory free for the vertics
     void Clear();
 
-    /// print out the graph as matrix form
+    // print out the graph as matrix form
     void Print(std::ofstream& fout);
 
-    /// check whether the graph has negative edge or not.
+    // check whether the graph has negative edge or not.
     bool IsNegativeEdge();
 
-    /// find the path from startVertexKey to endVertexKey with DFS (stack)
+    // find the path from startVertexKey to endVertexKey with DFS (stack)
     std::vector<int> FindPathBfs(int startVertexKey, int endVertexKey);
 
-    /// find the shortest path from startVertexKey to endVertexKey with Dijkstra using std::set
+    // find the shortest path from startVertexKey to endVertexKey with Dijkstra using std::set
     std::vector<int> FindShortestPathDijkstraUsingSet(int startVertexKey, int endVertexKey);
 
-    /// find the shortest path from startVertexKey to endVertexKey with Dijkstra using MinHeap
+    // find the shortest path from startVertexKey to endVertexKey with Dijkstra using MinHeap
     std::vector<int> FindShortestPathDijkstraUsingMinHeap(int startVertexKey, int endVertexKey);
 
-    /// find the shortest path from startVertexKey to endVertexKey with Bellman-Ford
+    // find the shortest path from startVertexKey to endVertexKey with Bellman-Ford
     std::vector<int> FindShortestPathBellmanFord(int startVertexKey, int endVertexKey);
 
     std::vector<vector<int> > FindShortestPathFloyd();
